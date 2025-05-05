58eed46049f4bcfe9a31c5881b8eef47db6cc050
Yotaka k. <88yotkha@gafe.molndal.se>
2025-05-05 15:50:24 +0200

    Add MongoDB configuration and services to the application

--- src/ProductApi.WebUI/Program.cs
+++ src/ProductApi.WebUI/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<IProductRepository, MongoProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

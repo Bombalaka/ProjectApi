911e44c75e06f3c391339d412eff62d60b8b02c3
Yotaka k. <88yotkha@gafe.molndal.se>
2025-05-05 15:33:11 +0200

    Implement Product entity and repository interface; remove unused classes and update project references

---
+++ src/ProductApi.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+using ProductApi.Domain.Entities;
+
+namespace ProductApi.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(int id);
+        Task<Product> AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+}

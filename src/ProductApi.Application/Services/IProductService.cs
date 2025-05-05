c4d43473ec5ac6e72f66a5b69ad1033dd4906cc4
Yotaka k. <88yotkha@gafe.molndal.se>
2025-05-05 15:35:50 +0200

    Add IProductService interface and ProductService implementation for product management

---
+++ src/ProductApi.Application/Services/IProductService.cs
@@ -0,0 +1,13 @@
+using ProductApi.Domain.Entities;
+
+namespace ProductApi.Application.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(int id);
+        Task<Product> CreateAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+}
